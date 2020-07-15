2d714969d7ba870ecd15b81bbde46c5f5aca0250
Kris Bosland <krisbbbprintportal@gmail.com>
2020-07-15 11:22:44 +1000

    Extra journey files that were not added earlier

---
+++ src/Presentation/Nop.Web/Models/Journey/JourneyModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Nop.Core.Domain.Catalog;
+using Nop.Core.Domain.Orders;
+using Nop.Web.Framework.Mvc.ModelBinding;
+using Nop.Web.Framework.Models;
+using Nop.Web.Models.Media;
+
+
+// Porttomis Inc. - Created for Journey
+namespace Nop.Web.Models.Journey
+{
+    public partial class JourneyModel : BaseNopEntityModel
+    {
+        public JourneyModel()
+        {
+            PictureModel = new PictureModel();
+        }
+
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Type { get; set; }
+        public string SeName { get; set; }
+        
+        public PictureModel PictureModel { get; set; }
+
+
+    }
+}
